9c763c77de942d8fdd97970d6e68dac2b1e08f56
Hao Lian <me@haolian.org>
2014-07-27 03:45:14 -0400

    Formatting API!

--- Main.hs
+++ Main.hs
@@ -2,11 +2,12 @@
 -- If writing Slack bots intrigues you, check out: https://github.com/hlian/linklater
 import BasePrelude hiding (words, intercalate, filter)
 import Control.Lens ((^.))
+import Data.Aeson (encode)
 import Data.Attoparsec.Text.Lazy
 import Data.Char (isLetter, isAscii)
 import Data.Text.Lazy
 import Data.Text.Lazy.Encoding (decodeUtf8)
-import Network.Linklater (say, slashSimple, Channel(..), Command(..), User(..), Config(..), Message(..), Icon(..))
+import Network.Linklater (say, slashSimple, Command(..), Config(..), Message(..), Icon(..), Format(..))
 import Network.Wai.Handler.Warp (run)
 import Network.Wreq hiding (params)
 
@@ -14,17 +15,17 @@ findUrl :: Text -> Maybe Text
 findUrl = fmap fromStrict . maybeResult . parse (manyTill (notChar '\n') (string "src=\"") *> takeTill (== '"'))
 
 jpgto :: Maybe Command -> IO Text
-jpgto (Just (Command (User user) channel (Just text))) = do
-  message <- (fmap message . findUrl . decodeUtf8 . flip (^.) responseBody) <$> get ("http://" <> (unpack subdomain) <> ".jpg.to/")
+jpgto (Just (Command user channel (Just text))) = do
+  message <- (fmap messageOf . findUrl . decodeUtf8 . flip (^.) responseBody) <$> get ("http://" <> (unpack subdomain) <> ".jpg.to/")
   case (debug, message) of
-    (True, _) -> putStrLn ("+ Pretending to post " <> show message) >> return ""
+    (True, _) -> putStrLn ("+ Pretending to post " <> (unpack . decodeUtf8 . encode) message) >> return ""
     (False, Just m) -> config' >>= say m >> return ""
     (False, Nothing) -> return "Something went wrong!"
   where config' = (Config "trello.slack.com" . filter (/= '\n') . pack) <$> readFile "token"
         subdomain = (intercalate "." . fmap (filter isLetter . filter isAscii) . words) text
-        message url = Message (EmojiIcon "gift") channel False (mconcat ["<@", user, "|", user, ">", " <", url, "|", subdomain, ".jpg.to>"])
+        messageOf url = FormattedMessage (EmojiIcon "gift") "jpgtobot" channel [FormatAt user, FormatLink url (subdomain <> ".jpg.to>")]
         debug = False
-jpgto _ = return "Type more! (Did you know? jpgtobot is only 27 lines of Haskell. <https://github.com/hlian/jpgtobot/blob/master/Main.hs>)"
+jpgto _ = return "Type more! (Did you know? jpgtobot is only 26 lines of Haskell. <https://github.com/hlian/jpgtobot/blob/master/Main.hs>)"
 
 main :: IO ()
 main = let port = 3000 in putStrLn ("+ Listening on port " <> show port) >> run port (slashSimple jpgto)
