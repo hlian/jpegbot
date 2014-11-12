aa25644d90022dd873478a57ff3058cccdb4a37e
Hao Lian <me@haolian.org>
2014-11-12 14:45:38 -0500

    Now with jpg.to /options support

--- Main.hs
+++ Main.hs
@@ -1,31 +1,90 @@
 {-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
+
 -- If writing Slack bots intrigues you, check out: https://github.com/hlian/linklater
+
 import BasePrelude hiding (words, intercalate, filter)
 import Control.Lens ((^.))
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
 import Data.Aeson (encode)
-import Data.Attoparsec.Text.Lazy
+import Data.ByteString.Lazy (ByteString)
 import Data.Char (isAlphaNum, isAscii)
-import Data.Text.Lazy
 import Data.Text.Lazy.Encoding (decodeUtf8)
-import Network.Linklater (say, slashSimple, Command(..), Config(..), Message(..), Icon(..), Format(..))
+import Network.HTTP.Base (urlEncode)
 import Network.Wai.Handler.Warp (run)
+
+-- Naked imports.
+import Data.Attoparsec.Text.Lazy
+import Data.Text.Lazy
+import Network.Linklater
 import Network.Wreq hiding (params)
 
-findUrl :: Text -> Maybe Text
-findUrl = fmap fromStrict . maybeResult . parse (manyTill (notChar '\n') (string "src=\"") *> takeTill (== '"'))
+findUrl :: ByteString -> Maybe Text
+findUrl =
+  encode . parse (manyTill (notChar '\n') (string "src=\"") *> takeTill (== '"')) . decode
+  where
+    encode =
+      fmap fromStrict . maybeResult
+    decode =
+      decodeUtf8
+
+configIO :: IO Config
+configIO = do
+  token <- (filter (/= '\n') . pack) <$> readFile "token"
+  return $ Config "trello.slack.com" token
+
+urlOf :: Text -> Text -> String
+urlOf query options =
+  "http://" <> urlEncode (unpack query) <> ".jpg.to/" <> unpack options
+
+parseText :: Text -> Maybe (Text, Text)
+parseText text =
+  f (strip <$> (splitOn "--" text))
+  where
+    f []             = mzero
+    f [raw]          = return (parseRaw raw, "")
+    f [raw, options] = return (parseRaw raw, options)
+    f _              = mzero
+    parseRaw         = intercalate "." . words
+
+liftMaybe :: Maybe a -> MaybeT IO a
+liftMaybe = maybe mzero return
+
+messageOfCommand :: Command -> MaybeT IO Message
+messageOfCommand (Command _ _ (Nothing)) =
+  mzero
+messageOfCommand (Command user channel (Just text)) = do
+  (query, options) <- liftMaybe $ parseText text
+  response <- liftIO $ get (urlOf query options)
+  let formatsOf url = [FormatAt user, FormatLink url (query <> ".jpg.to/" <> options)]
+  liftMaybe $ (messageOf . formatsOf) <$> findUrl (response ^. responseBody)
+  where
+    messageOf =
+      FormattedMessage (EmojiIcon "gift") "jpgtobot" channel
 
 jpgto :: Maybe Command -> IO Text
-jpgto (Just (Command user channel (Just text))) = do
-  message <- (fmap messageOf . findUrl . decodeUtf8 . (^. responseBody)) <$> get ("http://" <> (unpack subdomain) <> ".jpg.to/")
+jpgto Nothing = do
+  return "Unrecognized Slack request!"
+
+jpgto (Just command) = do
+  putStrLn ("+ Incoming command: " <> show command)
+  config <- configIO
+  message <- (runMaybeT . messageOfCommand) command
   case (debug, message) of
-    (True, _) -> putStrLn ("+ Pretending to post " <> (unpack . decodeUtf8 . encode) message) >> return ""
-    (False, Just m) -> config' >>= say m >> return ""
-    (False, Nothing) -> return ("No JPEG found for " <> subdomain <> ", sorry!")
-  where config' = (Config "trello.slack.com" . filter (/= '\n') . pack) <$> readFile "token"
-        subdomain = (intercalate "." . fmap (filter isAlphaNum. filter isAscii) . words) text
-        messageOf url = FormattedMessage (EmojiIcon "gift") "jpgtobot" channel [FormatAt user, FormatLink url (subdomain <> ".jpg.to")]
-        debug = False
-jpgto _ = return "Type more! (Did you know? jpgtobot is only 26 lines of Haskell. <https://github.com/hlian/jpgtobot/blob/master/Main.hs>)"
+    (True, _) -> do
+      putStrLn ("+ Outgoing messsage: " <> show (encode <$> message))
+      return ""
+    (False, Just m) -> do
+      say m config
+      return ""
+    (False, Nothing) ->
+      return "*FRIZZLE* ERROR PROCESSING INPUT; BEGIN SELF-DETONATION; PLEASE FILE ISSUE AT <https://github.com/hlian/jpgtobot>"
+  where
+    debug = False
 
 main :: IO ()
-main = let port = 3000 in putStrLn ("+ Listening on port " <> show port) >> run port (slashSimple jpgto)
+main = do
+  putStrLn ("+ Listening on port " <> show port)
+  run port (slashSimple jpgto)
+    where
+      port = 3333
