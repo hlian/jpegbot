980ecf303a23b48eb245e2814859b3d5688c38e5
Hao Lian <hao@trello.com>
2015-07-06 10:49:08 -0400

    use doug's google search library

--- Main.hs
+++ Main.hs
@@ -15,6 +15,7 @@ import           Data.Char (isAlphaNum, isAscii)
 import           Data.Text (Text)
 import           Data.Text.Lazy.Encoding (decodeUtf8)
 import           Network.HTTP.Base (urlEncode)
+import           Network.Images.Search as Search
 import           Network.Wai.Handler.Warp (run)
 
 -- Naked imports.
@@ -23,50 +24,41 @@ import           Data.Attoparsec.Text.Lazy
 import           Network.Linklater
 import           Network.Wreq hiding (params)
 
-findURL :: ByteString -> Maybe Text
-findURL =
-  encode . parse (manyTill (notChar '\n') (string "src=\"") *> takeTill (== '"')) . decode
-  where
-    encode =
-      maybeResult
-    decode =
-      decodeUtf8
+cleverlyReadFile filename =
+  T.filter (/= '\n') . T.pack <$> readFile filename
 
 configIO :: IO Config
 configIO =
-  Config <$> (T.filter (/= '\n') . T.pack <$> readFile "hook")
+  Config <$> (cleverlyReadFile "hook")
 
-urlOf :: Text -> Text -> String
-urlOf query path =
-  "http://" <> urlEncode (T.unpack query) <> ".jpg.to/" <> T.unpack path
+googleConfigIO :: IO Search.Gapi
+googleConfigIO =
+  Search.config <$> (cleverlyReadFile "google-server-key") <*> (cleverlyReadFile "google-search-engine-id")
 
-parseText :: Text -> Maybe (Text, Text)
-parseText text =
-  f (filter (/= "") (T.strip <$> T.splitOn "--" text))
-  where
-    f []             = mzero
-    f [raw]          = return (parseRaw raw, "")
-    f [raw, options] = return (parseRaw raw, "/" <> options)
-    f _              = mzero
-    parseRaw         = T.intercalate "." . T.words
+parseText :: Text -> Maybe Text
+parseText text = case T.strip text of
+  "" -> Nothing
+  x -> Just x
 
 liftMaybe :: Maybe a -> MaybeT IO a
 liftMaybe = maybe mzero return
 
 messageOfCommand :: Command -> MaybeT IO Message
-messageOfCommand (Command "jpeg" _ _ (Nothing)) =
+
+messageOfCommand (Command "jpeg" _ _ Nothing) =
   mzero
 messageOfCommand (Command "jpeg" user channel (Just text)) = do
-  (query, path) <- liftMaybe (parseText text)
-  response <- liftIO (get (urlOf query path))
-  url <- liftMaybe (findURL $ response ^. responseBody)
-  return (messageOf [FormatAt user, FormatLink url (query <> ".jpg.to" <> path)])
+  gapi <- liftIO googleConfigIO
+  query <- liftMaybe (parseText text)
+  urls <- liftIO (Search.linksOfQuery gapi query)
+  url <- liftMaybe (listToMaybe urls)
+  return (messageOf [FormatAt user, FormatLink url url])
   where
     messageOf =
       FormattedMessage (EmojiIcon "gift") "jpgtobot" channel
 
 jpgto :: Maybe Command -> IO Text
-jpgto Nothing = do
+jpgto Nothing =
   return "Unrecognized Slack request!"
 
 jpgto (Just command) = do
